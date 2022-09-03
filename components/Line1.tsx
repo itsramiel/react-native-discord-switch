9323f435ab555396f8bfbf40ae495e25b8a28271
ramiel <ramielwan48@gmail.com>
2022-09-03 11:05:44 +0300

    lines animate halfway

---
+++ components/Line1.tsx
@@ -0,0 +1,56 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { COLORS, SIZE } from "../constants";
+import Animated, {
+  interpolate,
+  useAnimatedStyle,
+} from "react-native-reanimated";
+
+const LINE_LENGTH = SIZE.THUMB_HEIGHT - 2 * SIZE.PADDING;
+const LINE_THICKNESS = 5;
+const START_TRANSLATION_X =
+  SIZE.THUMB_START_WIDTH / 2 - SIZE.PADDING - LINE_THICKNESS / 2;
+const START_ROTATION = -45;
+
+const MIDDLE_TRANSLATION_X =
+  SIZE.THUMB_START_WIDTH / 2 - SIZE.PADDING - LINE_THICKNESS / 2;
+const MIDDLE_ROTATION = -90;
+
+interface Line1Props {
+  animated: Animated.SharedValue<0 | 1>;
+}
+
+const Line1 = ({ animated }: Line1Props) => {
+  const rStyle = useAnimatedStyle(() => {
+    return {
+      position: "absolute",
+      top: SIZE.PADDING,
+      left: SIZE.PADDING,
+      height: LINE_LENGTH,
+      width: LINE_THICKNESS,
+      backgroundColor: COLORS.GRAY_LIGHT,
+      transform: [
+        {
+          translateX: interpolate(
+            animated.value,
+            [0, 1],
+            [START_TRANSLATION_X, MIDDLE_TRANSLATION_X]
+          ),
+        },
+        {
+          rotate: `${interpolate(
+            animated.value,
+            [0, 1],
+            [START_ROTATION, MIDDLE_ROTATION]
+          )}deg`,
+        },
+      ],
+    };
+  }, []);
+
+  return <Animated.View style={rStyle} />;
+};
+
+export default Line1;
+
+const styles = StyleSheet.create({});
--- components/Thumb.tsx
+++ components/Thumb.tsx
@@ -5,6 +5,8 @@ import Animated, {
   useAnimatedStyle,
 } from "react-native-reanimated";
 import { COLORS, SIZE } from "../constants";
+import Line1 from "./Line1";
+import Line2 from "./Line2";
 
 const {
   THUMB_START_WIDTH,
@@ -12,6 +14,7 @@ const {
   THUMB_HEIGHT,
   THUMB_START_TRANSLATION,
   THUMB_FINAL_TRANSLATION,
+  PADDING,
 } = SIZE;
 const { WHITE } = COLORS;
 
@@ -30,6 +33,7 @@ const Thumb = ({ animated }: ThumbProps) => {
       height: THUMB_HEIGHT,
       borderRadius: THUMB_HEIGHT / 2,
       backgroundColor: WHITE,
+      padding: PADDING,
       transform: [
         {
           translateX: interpolate(
@@ -42,9 +46,12 @@ const Thumb = ({ animated }: ThumbProps) => {
     };
   }, [animated]);
 
-  return <Animated.View style={rStyle} />;
+  return (
+    <Animated.View style={rStyle}>
+      <Line1 animated={animated} />
+      <Line2 animated={animated} />
+    </Animated.View>
+  );
 };
 
 export default Thumb;
-
-const styles = StyleSheet.create({});
